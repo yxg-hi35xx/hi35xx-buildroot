2ae995904e21af31383f4910e981cb2dd95d62a0
Watson Xu <xuhuashan@gmail.com>
2017-04-11 11:46:38 +0800

    automatically firmware update from SD/MMC
    when U-Boot found "update.scr" on the root directory of SD/MMC card,
    it will be loaded as a firmware update script, and run this script.

--- board/yxgtech/hi3518v200/post-build.sh
+++ board/yxgtech/hi3518v200/post-build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 BOARD_DIR="$(dirname $0)"
+MKIMAGE=$HOST_DIR/usr/bin/mkimage
 
 # update build time to /etc/version
 version=`sed -nr 's/^version=(.*)$/\1/p' \
@@ -10,3 +11,7 @@ localver=`support/scripts/setlocalversion`
 echo "localver=${localver#-}" >> ${TARGET_DIR}/etc/version
 build=`date "+%Y%m%d%H%M%S"`
 echo "build=${build}" >> ${TARGET_DIR}/etc/version
+
+# Generate firmware update script for U-Boot
+$MKIMAGE -C none -A arm -T script -d ${BOARD_DIR}/update.txt \
+	${BINARIES_DIR}/update.scr
