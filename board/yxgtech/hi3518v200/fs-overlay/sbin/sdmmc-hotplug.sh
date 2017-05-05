b5c0e2c728863b845400c25454726e36fe4d7e5a
Watson Xu <xuhuashan@gmail.com>
2017-05-05 13:15:51 +0800

    board/yxgtech/snapshotd: add snapshotd package to take pictures

--- board/yxgtech/hi3518v200/fs-overlay/sbin/sdmmc-hotplug.sh
+++ board/yxgtech/hi3518v200/fs-overlay/sbin/sdmmc-hotplug.sh
@@ -2,21 +2,31 @@
 
 [ -n "$ACTION" -a -n "$MDEV" ] || exit 1
 
+SNAPSHOTD_PID=/var/run/snapshotd.pid
+SNAPSHOT_URI=rtsp://127.0.0.1/snapshot
+
 case ${ACTION} in
   add)
     (
-      mount | grep -q "^/dev/$MDEV" && exit 0
+      grep -q "^/dev/$MDEV" /proc/mounts && exit 0
       usleep 100000
-      mount | grep -q "^/dev/$MDEV" && exit 0
+      grep -q "^/dev/$MDEV" /proc/mounts && exit 0
       [ -b "/dev/$MDEV" ] || exit 1
       mnt=/run/media/$MDEV
       [ -d "$mnt" ] || mkdir -p "$mnt"
       fstype=$(blkid "/dev/${MDEV}" | sed -nr 's/.*TYPE=\"([^\"]*)\".*/\1/p')
       mount -t "$fstype" "/dev/${MDEV}" "${mnt}" || exit 1
+      [ -x /usr/bin/snapshotd ] && {
+        start-stop-daemon -S -q -m -b -p $SNAPSHOTD_PID \
+            -x /usr/bin/snapshotd -- -s -p "$mnt/photo/" "$SNAPSHOT_URI"
+      }
     ) &
     ;;
   remove)
     (
+      [ -x /usr/bin/snapshotd ] && {
+        start-stop-daemon -K -q -p $SNAPSHOTD_PID
+      }
       mnt=$(mount | awk "(\$1 ~ /\/dev\/$MDEV/) { print \$3 }")
       [ -n "$mnt" ] && umount "$mnt" && rm -r "$mnt"
     ) &
